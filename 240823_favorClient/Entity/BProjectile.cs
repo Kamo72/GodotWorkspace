e3b372294232cc024a1558c8bc3e0d091e7226d6
kamo72 <agag3932@gmail.com>
2024-09-06 17:44:56 +0900

    240906_18commit

---
+++ 240823_favorClient/Entity/BProjectile.cs
@@ -0,0 +1,25 @@
+﻿using _favorClient.library.DataType;
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _favorClient.Entity
+{
+    public partial class BProjectile : RigidBody2D
+    {
+
+        public PDamage damage;
+
+        public virtual List<Character> GetCollide()
+        {
+            //OverrideNeeded
+
+            return new();
+        }
+
+
+    }
+}
---
+++ 240823_favorClient/Entity/PProjectile.cs
@@ -0,0 +1,27 @@
+﻿using _favorClient.library.DataType;
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _favorClient.Entity
+{
+    public partial class PProjectile : RigidBody2D
+    {
+        public BDamage damage;
+
+        public virtual Boss GetCollide() 
+        {
+            //OverrideNeeded
+            return null;
+        }
+
+
+
+
+
+
+    }
+}
